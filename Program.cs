27775ba78cba23c39db9f25bcc0de1a297d4c88e
KUMARIG <kumarig@amdocs.com>
2021-07-24 10:53:52 +0530

    BarclaysTest

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = args[1];
+            Console.WriteLine("trying path: " + path);
+
+            FileDetails fd = new FileDetails();
+            Console.WriteLine(fd.Version(path));
+            Console.WriteLine(fd.Size(path));
+            Console.Read();
+        }
+    }
+}
